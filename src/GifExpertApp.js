622d0f82bed285f0f12e95df2533e31dc7948600
hernan066 <hernan0066@gmail.com>
2021-09-01 12:29:32 -0300

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,13 @@
+//rafc es para crear el cascaron del componente
+import React from 'react'
+
+export const GifExpertApp = () => {
+    return (
+        <>
+           <h2>Gif Expert App</h2> 
+           <hr />
+        </>
+    )
+}
+
+export default GifExpertApp;
